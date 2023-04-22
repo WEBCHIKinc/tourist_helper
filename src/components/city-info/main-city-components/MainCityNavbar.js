4a40095a9b691c1eebf95e884e44235b86ac2581
WEB_inc <kozerenkod@gmail.com>
2023-04-22 22:18:43 +0300

    city-nav-button, css class for each button deleted

--- src/components/city-info/main-city-components/MainCityNavbar.js
+++ src/components/city-info/main-city-components/MainCityNavbar.js
@@ -4,8 +4,8 @@ import './mcc.scss'
  export const MainCityNavbar = () => {
   return (
     <div className="nav-bar">
-      <button className="button1"> Converter</button>
-      <button className="button2"> Weather forecast</button>
+      <button className="city-nav-button"> Converter</button>
+      <button className="city-nav-button"> Weather forecast</button>
     </div>
   );
 };
