ae079272a9d93d5dde9003aad9f453fdf9c83611
WEB_inc <kozerenkod@gmail.com>
2023-05-22 00:08:29 +0300

    navbar buttons optimization

--- src/components/city-navbar/CityNavbar.js
+++ src/components/city-navbar/CityNavbar.js
@@ -1,26 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 export const CityNavbar = ({ pageType, setPageType }) => {
-  const [buttons, setButtons] = useState(['Weather', 'Converter']);
-
-  useEffect(() => {
-    if (pageType === 'Converter') {
-      setButtons(['Weather', 'Info']);
-    } else if (pageType === 'Weather') {
-      setButtons(['Converter', 'Info']);
-    } else {
-      setButtons(['Weather', 'Converter']);
-    }
-  }, [pageType]);
-
   function onButtonClick(e) {
     setPageType(e.target.textContent);
   }
 
   return (
     <div className="city-nav-bar">
-      <button className="city-nav-button text" onClick={onButtonClick}>{buttons[0]}</button>
-      <button className="city-nav-button text" onClick={onButtonClick}>{buttons[1]}</button>
+      <button className={`city-nav-button text ${pageType === 'Weather' ? 'hidden' : ''}`} onClick={onButtonClick}>Weather</button>
+      <button className={`city-nav-button text ${pageType === 'Converter' ? 'hidden' : ''}`} onClick={onButtonClick}>Converter</button>
+      <button className={`city-nav-button text ${pageType === 'Info' ? 'hidden' : ''}`} onClick={onButtonClick}>Info</button>
     </div>
   );
 };
